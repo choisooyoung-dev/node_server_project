a3caade5defb33cc60cae10c912b8a5e4062fe62
sooyoung.choi <sooyoung.choi.dev@gmail.com>
2023-11-13 00:56:20 +0900

    feat: Implements login, user info

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { Users } = require("../models");
-console.log(Users);
+// console.log(Users);
 
 const router = express.Router();
 
@@ -15,7 +15,7 @@ router.post("/users", async (req, res) => {
         return res.status(409).json({ message: "이미 존재하는 이메일입니다." });
     }
 
-    const user = await Users.create({ email, password, username });
+    await Users.create({ email, password, username });
 
     return res.status(201).json({ message: "회원가입이 완료되었습니다." });
 });
@@ -24,16 +24,28 @@ router.post("/users", async (req, res) => {
 router.post("/login", async (req, res) => {
     const { email, password } = req.body;
     const user = await Users.findOne({ where: { email } });
+    // console.log(user.password);
+    // console.log(typeof password);
 
     if (!user) {
         return res.status(401).json({ message: "존재하지 않는 이메일입니다." });
-    } else if (user.password !== password) {
+    } else if (Number(user.password) !== password) {
         return res
             .status(401)
-            .status({ message: "비밀번호가 일치하지 않습니다." });
+            .json({ message: "비밀번호가 일치하지 않습니다." });
     }
 
-    return res.status(201).json({ message: "로그인 성공" });
+    return res.status(200).json({ message: "로그인 성공" });
 });
 
 module.exports = router;
+
+// read user info
+router.get("/users/:userId", async (req, res) => {
+    const { userId } = req.params;
+    const user = await Users.findOne({
+        attributes: ["userId", "email", "username"],
+        where: { userId },
+    });
+    return res.status(200).json({ data: user });
+});
